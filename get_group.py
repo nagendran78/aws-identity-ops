7ac0d2fe9c77a15c212d92d73c3bcd71fa7d04ea
Nagendran Sandraprakasam <nagen78@gmail.com>
2024-12-03 16:20:01 +0800

    Group feature

---
+++ get_group.py
@@ -0,0 +1,47 @@
+"""
+Author: nagendran.s
+Description: Retrieve All the Groups and Users associated to it
+             from "AWS-IAM Identity Center"
+"""
+
+import boto3
+import uuid
+from src.groups import all_group
+from src.groups import export_groups
+
+# AWS Payer CLI Profile name
+aws_profile_name            = '' 
+
+# IAM Identity Center - Store ID
+identity_centre_store_id    = ''
+
+#Output File Name
+output_file_name            = "group-" + uuid.uuid4().hex + ".csv"
+
+# Method Execution Section
+if __name__ == "__main__":
+    # Create a session using the specified AWS profile
+    session = boto3.Session(profile_name = aws_profile_name)
+
+    # Create an identitystore client from the session
+    identitystore_client = session.client('identitystore')
+
+    # Fetch all the Groups
+    groups_data = dict()
+    groups_data = all_group.get_all_groups(identity_centre_store_id, identitystore_client)
+
+    # region Print Group-Name and UserName details on the screen
+    """
+    for group_name, username in groups_data.items():
+        print(f"Group: {group_name}")
+        print(f"Username: {', '.join(username)}")
+        print("-" * 40)  # Separator for better readability
+    """
+    # endregion
+        
+    # Export Groups data to CSV 
+    export_groups.export_groups_to_csv(groups_data, output_file_name)
+
+    #Notify the process has been completed.
+    print(f"Completed fetching Groups", "\U0001F60E")
+    print(f"Output File Name: {output_file_name}")
--- src/groups/__init__.py
+++ src/groups/__init__.py
@@ -8,19 +8,25 @@ file imports key functions to expose them as part of the package's public API.
 
 Exposed API:
 - get_groups_for_user: Retrieve Groups for a specific user from AWS Identity Center.
+- get_all_groups: Retrieve Groups and Users.EmailID.
+- export_groups_to_csv: Export Groups and associated users to CSV file format.
 
 Modules:
-- group_user: Handles retrieval of Groups for specific user 
+- groups: Handles retrieval of all Groups and associated Users 
 
 Usage:
 Import specific function or modules as needed:
     from src.groups import group_user
+    from src.groups import all_group
+    from src.groups import export_groups
 """
 
 # src/groups/__init__.py
 # Import specific function
 from .group_user import get_groups_for_user
+from .all_group import get_all_groups
+from .export_groups import export_groups_to_csv
 
 # Specifying explicity Public API that should be exposed
-__all__ = ["get_groups_for_user"]
+__all__ = ["get_groups_for_user", "get_all_groups", "export_groups_to_csv"]
 print("Initializing groups package...")
